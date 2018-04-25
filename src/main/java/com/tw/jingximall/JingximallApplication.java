e0215ef4d65b0ed2de9759e3b6cd2626da5807ee
wangjie <728199993@qq.com>
2018-04-25 20:09:09 +0800

    first commit

---
+++ src/main/java/com/tw/jingximall/JingximallApplication.java
@@ -0,0 +1,12 @@
+package com.tw.jingximall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JingximallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JingximallApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tw/jingximall/JingximallApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tw.jingximall;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JingximallApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
