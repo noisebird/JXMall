6fb4c12ed454540961c06ba94ea0f82eb100e119
wangjie <728199993@qq.com>
2018-05-09 19:37:01 +0800

    完成了添加商品的业务方法

---
+++ src/main/java/com/tw/jingximall/service/ProductSericeImpl.java
@@ -0,0 +1,25 @@
+package com.tw.jingximall.service;
+
+import com.tw.jingximall.entity.Inventory;
+import com.tw.jingximall.entity.Product;
+import com.tw.jingximall.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by wangjie on 2018/5/9.
+ */
+@Service
+public class ProductSericeImpl {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Product addProduct(Product product) {
+        product.setInventory(new Inventory());
+        return productRepository.saveAndFlush(product);
+    }
+
+
+}
